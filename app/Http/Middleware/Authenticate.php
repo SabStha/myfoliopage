3d85af0b3657890e40aaa625e86755903acc3d93
Sabin Shrestha <sabstha98@gmail.com>
2025-11-12 17:12:02 +0900

    sixth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -11,25 +11,6 @@ use Closure;
 class Authenticate extends Middleware
 {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string[]  ...$guards
-     * @return mixed
-     *
-     * @throws \Illuminate\Auth\AuthenticationException
-     */
-    /**
-     * Handle an unauthenticated user.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  array  $guards
-     * @return void
-     *
-     * @throws \Illuminate\Auth\AuthenticationException
-     */
-    /**
      * Handle an unauthenticated user.
      *
      * @param  \Illuminate\Http\Request  $request
