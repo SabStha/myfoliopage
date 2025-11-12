75ce663f82d3ffeb1403ad1be3a2b6bc5fd26c07
Sabin Shrestha <sabstha98@gmail.com>
2025-11-12 14:40:17 +0900

    third push

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\SetLocale::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withSchedule(function (Schedule $schedule): void {
         // No scheduled tasks needed for simple LinkedIn integration
